4a97aeb6db141d6a34dc1f00284bd4c8e83f43dd
Israel Araujo <isra.rar@icloud.com>
2019-03-13 15:37:03 -0300

    novos testes com a porta 3001

--- test/jest.test.js
+++ test/jest.test.js
@@ -1,4 +1,20 @@
 test('Deve conhecer as principais assertivas do jest', () => {
     let number = null;
     expect(number).toBeNull();
+    number = 10;
+    expect(number).not.toBeNull();
+    expect(number).toBe(10);
+    expect(number).toEqual(10);
+    expect(number).toBeGreaterThan(9);
+    expect(number).toBeLessThan(11);
+});
+
+test('Devo saber trabalhar com objetos', () => {
+    const obj = { name: "John", email: "john@gmail.com"};
+    expect(obj).toHaveProperty('name');
+    expect(obj).toHaveProperty('name', 'John');
+    expect(obj.email).toBe('john@gmail.com');
+
+    const obj2 = { name: "John", email: "john@gmail.com"};
+    expect(obj).toEqual(obj2);
 });
