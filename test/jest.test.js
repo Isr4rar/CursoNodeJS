8cce83393318aee7cae42064e59b80ee422ad18e
Israel Araujo <isra.rar@icloud.com>
2019-03-18 22:11:24 -0300

    Aula 23 concluida

--- test/jest.test.js
+++ test/jest.test.js
@@ -1,17 +1,17 @@
 test('Deve conhecer as principais assertivas do jest', () => {
     let number = null;
-    expect(number).toBeNull();
+    expect(number).toBeNull(); //validar se é null
     number = 10;
-    expect(number).not.toBeNull();
-    expect(number).toBe(10);
-    expect(number).toEqual(10);
-    expect(number).toBeGreaterThan(9);
-    expect(number).toBeLessThan(11);
+    expect(number).not.toBeNull(); //validar se não é null
+    expect(number).toBe(10);//validar se é 10
+    expect(number).toEqual(10);//validar se é 10
+    expect(number).toBeGreaterThan(9); //validar se é maior que 9
+    expect(number).toBeLessThan(11); //validar se é menor que 11
 });
 
 test('Devo saber trabalhar com objetos', () => {
     const obj = { name: "John", email: "john@gmail.com"};
-    expect(obj).toHaveProperty('name');
+    expect(obj).toHaveProperty('name'); //validar propriedade "name"
     expect(obj).toHaveProperty('name', 'John');
     expect(obj.email).toBe('john@gmail.com');
 
--- test/user.test.js
+++ test/user.test.js
@@ -6,16 +6,17 @@ test('Deve listar todos os usuarios', () => {
     return request(app).get('/users')
     .then(res => {
         expect(res.status).toBe(200);
-        expect(res.body).toHaveLength(1);
-        expect(res.body[0]).toHaveProperty('name', 'John Doe');
+        expect(res.body).toHaveLength(2);
+        expect(res.body[0]).toHaveProperty('name', 'Israel Araujo');
+        expect(res.body[1]).toHaveProperty('name', 'Lais Silva');
     });
 });
 
 test('Deve inserir usuário com sucesso', () => {
     return request(app).post('/users')
-        .send({name: 'Walter Mitty', mail:'walter@mail.com'})
+        .send({name: 'Gabriel Araujo', mail:'gabriel@mail.com'})
         .then(res => {
             expect(res.status).toBe(201);
-            expect(res.body.name).toBe('Walter Mitty');
+            expect(res.body.mail).toBe('gabriel@mail.com');
         });
 });
